30d2159834ea9d68e21d433e6dd368400ab8a7fb
Oleksandr Kovalenko <okovalenko@lenovo.com>
2020-06-27 14:04:38 +0300

    Refactoring

--- js/note.js
+++ js/note.js
@@ -1,3 +1,21 @@
+// Constraints
+const CHECK_ICON = 'fa fa-check-circle co';
+const UNCHECK_ICON = 'fa fa-circle-thin co';
+const TRASH_ICON = 'fa fa-trash-o de';
+const EDIT_ICON = 'fa fa-edit ed';
+const ACTION_EDIT = 'edit';
+const ACTION_REMOVE = 'remove';
+const ACTION_COMPLETE = 'complete';
+const STATE_ACTIVE = 'active';
+const STATE_COMPLETED = 'completed';
+const ATTRIBUTE_ID = 'id';
+const ATTRIBUTE_ACTION = 'action';
+const EVENT_CLICK = 'click';
+const LS_NOTE_LIST = 'noteList';
+const CLASS_TEXT = 'text';
+const CLASS_ITEM = 'item';
+const CLASS_SHOW_MODAL = 'show-modal';
+
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 const date = document.getElementById("date");
@@ -17,10 +35,6 @@ let currentDate = new Date(Date.now());
 let noteType = '';
 let selectedNote = '';
 
-// Icons
-const CHECK_ICON = "fa fa-check-circle co";
-const UNCHECK_ICON = "fa fa-circle-thin co";
-
 class Note {
     constructor(noteId, state, text, date) {
         this.noteId = noteId;
@@ -31,7 +45,10 @@ class Note {
 }
 
 // Init app
-renderNotes()
+function intApp(){
+    renderNotes();
+    setDate(new Date());
+}
 
 //Handle localStorage content
 function renderNotes() {
@@ -43,46 +60,20 @@ function renderNotes() {
 }
 
 function saveNodeListToLocalStorage() {
-    localStorage.setItem("noteList", JSON.stringify(noteList));
+    localStorage.setItem(LS_NOTE_LIST, JSON.stringify(noteList));
 }
 
 function getNoteListFromLocalStorage() {
-    return localStorage.getItem("noteList") ? JSON.parse(localStorage.getItem("noteList")) : [];
+    return localStorage.getItem(LS_NOTE_LIST) ? JSON.parse(localStorage.getItem(LS_NOTE_LIST)) : [];
 }
 
 //handle date
-setDate(new Date());
-calendar.addEventListener("change", () => {
-    currentDate = new Date(calendar.value);
-    setDate(currentDate);
-    eraseRenderedNotes();
-    renderNotes()
-});
-today.addEventListener("click", () => {
-    currentDate = new Date(Date.now());
-    calendar.value = currentDate.toISOString().split('T')[0]
-    setDate(currentDate);
-    eraseRenderedNotes();
-    renderNotes()
-});
-
 function setDate(someDate) {
     const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     date.innerText = new Date(Date.parse(someDate)).toLocaleDateString(undefined, options);
 }
 
 // handle add note
-document.addEventListener("click", () => {
-    if (input.value) {
-        input.style.width = '100%';
-    }
-})
-input.addEventListener("keyup", (e) => {
-    if (e.key === 'Enter') {
-        handleAddNote();
-    }
-});
-
 function handleAddNote() {
     if (input.value === '\n') {
         input.value = '';
@@ -91,7 +82,7 @@ function handleAddNote() {
     if (noteType) {
         changeNoteType('');
     }
-    renderAndSaveNoteList(new Note(id, 'active', input.value, currentDate));
+    renderAndSaveNoteList(new Note(id, STATE_ACTIVE, input.value, currentDate));
     input.value = '';
 }
 
@@ -106,13 +97,13 @@ function renderAndSaveNoteList(note) {
 function renderNoteList(note) {
     if (note.text && (note.state === noteType || !noteType) && (new Date(Date.parse(note.date)).setHours(0, 0, 0, 0) === currentDate.setHours(0, 0, 0, 0))) {
         const noteElement = document.createElement("li");
-        noteElement.className = 'item';
+        noteElement.className = CLASS_ITEM;
         noteElement.id = note.noteId;
         noteElement.innerHTML = `
-                <i class="${note.state === 'active' ? UNCHECK_ICON : CHECK_ICON}" action="complete" id=${note.noteId}></i>
-                <p class="text">${note.text}</p>
-                <i class="fa fa-trash-o de" action="remove" id=${note.noteId}></i>
-                <i class="fa fa-edit ed" action="edit" id=${note.noteId}></i>
+                <i class='${note.state === STATE_ACTIVE ? UNCHECK_ICON : CHECK_ICON}' action=${ACTION_COMPLETE} id=${note.noteId}></i>
+                <p class='${CLASS_TEXT}'>${note.text}</p>
+                <i class='${TRASH_ICON}' action=${ACTION_REMOVE} id=${note.noteId}></i>
+                <i class='${EDIT_ICON}' action=${ACTION_EDIT} id=${note.noteId}></i>
 `
         list.insertAdjacentElement('beforeend', noteElement);
         window.scrollTo(0, document.body.scrollHeight);
@@ -125,46 +116,7 @@ function eraseRenderedNotes() {
     list.innerHTML = '';
 }
 
-//Handle change note event
-list.addEventListener("click", e => {
-        // const noteId = e.target.getAttribute('id');
-        const action = e.target.getAttribute('action');
-
-        if (action === 'complete') {
-            const target = document.getElementById(e.target.getAttribute('id'));
-            const state = e.target.className === CHECK_ICON ? UNCHECK_ICON : CHECK_ICON;
-            e.target.className = state;
-            noteList.find(note => note.noteId == target.id).state = state === CHECK_ICON ? 'completed' : 'active';
-            saveNodeListToLocalStorage();
-        }
-        if (action === 'remove') {
-            const target = document.getElementById(e.target.getAttribute('id'));
-            noteList.splice(noteList.findIndex(note => note.noteId == target.id), 1)
-
-            target.remove()
-            saveNodeListToLocalStorage();
-        }
-        if (action === 'edit') {
-            const target = document.getElementById(e.target.getAttribute('id'));
-            const text = target.getElementsByClassName('text')[0];
-            modalInput.innerText = text.innerText;
-            selectedNote = target.getAttribute("id");
-            toggleModal();
-        }
-    }
-);
-
 //handle change type note event
-active.addEventListener('click', () => {
-    changeNoteType('active');
-});
-completed.addEventListener('click', () => {
-    changeNoteType('completed')
-});
-all.addEventListener('click', () => {
-    changeNoteType('');
-});
-
 function changeNoteType(status) {
     noteType = status;
     eraseRenderedNotes();
@@ -172,16 +124,8 @@ function changeNoteType(status) {
 }
 
 //edit note modal
-submitButton.addEventListener("click", (e) => {
-    editNote(e);
-    toggleModal()
-});
-closeButton.addEventListener("click", toggleModal);
-
-window.addEventListener("click", windowOnClick);
-
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+    modal.classList.toggle(CLASS_SHOW_MODAL);
 }
 
 function windowOnClick(event) {
@@ -192,11 +136,90 @@ function windowOnClick(event) {
 
 function editNote(e) {
     const target = document.getElementById(selectedNote);
-    const text = target.getElementsByClassName('text')[0];
+    const text = target.getElementsByClassName(CLASS_TEXT)[0];
     if (modalInput.value && modalInput.value !== text.innerText) {
         text.innerText = modalInput.value;
-        noteList.find(note => note.noteId == selectedNote).text = text.innerText;
+        noteList.find(note => note.noteId.toString() === selectedNote).text = text.innerText;
         saveNodeListToLocalStorage();
     }
 }
 
+//Event listeners
+
+//initApp
+window.onload = () => intApp();
+
+//date
+calendar.addEventListener("change", () => {
+    currentDate = new Date(calendar.value);
+    setDate(currentDate);
+    eraseRenderedNotes();
+    renderNotes()
+});
+today.addEventListener(EVENT_CLICK, () => {
+    currentDate = new Date(Date.now());
+    calendar.value = currentDate.toISOString().split('T')[0]
+    setDate(currentDate);
+    eraseRenderedNotes();
+    renderNotes()
+});
+
+// add note
+document.addEventListener(EVENT_CLICK, () => {
+    if (input.value) {
+        input.style.width = '100%';
+    }
+})
+input.addEventListener("keyup", (e) => {
+    if (e.key === 'Enter') {
+        handleAddNote();
+    }
+});
+
+//changing note event
+list.addEventListener(EVENT_CLICK, e => {
+        const action = e.target.getAttribute(ATTRIBUTE_ACTION);
+
+        if (action === ACTION_COMPLETE) {
+            const target = document.getElementById(e.target.getAttribute(ATTRIBUTE_ID));
+            const state = e.target.className === CHECK_ICON ? UNCHECK_ICON : CHECK_ICON;
+            e.target.className = state;
+            noteList.find(note => note.noteId.toString() === target.id).state = state === CHECK_ICON ? STATE_COMPLETED : STATE_ACTIVE;
+            saveNodeListToLocalStorage();
+        }
+        if (action === ACTION_REMOVE) {
+            const target = document.getElementById(e.target.getAttribute(ATTRIBUTE_ID));
+            noteList.splice(noteList.findIndex(note => note.noteId.toString() === target.id), 1)
+
+            target.remove()
+            saveNodeListToLocalStorage();
+        }
+        if (action === ACTION_EDIT) {
+            const target = document.getElementById(e.target.getAttribute(ATTRIBUTE_ID));
+            const text = target.getElementsByClassName(CLASS_TEXT)[0];
+            modalInput.innerText = text.innerText;
+            selectedNote = target.getAttribute(ATTRIBUTE_ID);
+            toggleModal();
+        }
+    }
+);
+
+//Modal
+submitButton.addEventListener(EVENT_CLICK, (e) => {
+    editNote(e);
+    toggleModal()
+});
+closeButton.addEventListener(EVENT_CLICK, toggleModal);
+
+window.addEventListener(EVENT_CLICK, windowOnClick);
+
+//changing note type
+active.addEventListener(EVENT_CLICK, () => {
+    changeNoteType(STATE_ACTIVE);
+});
+completed.addEventListener(EVENT_CLICK, () => {
+    changeNoteType(STATE_COMPLETED)
+});
+all.addEventListener(EVENT_CLICK, () => {
+    changeNoteType('');
+});
